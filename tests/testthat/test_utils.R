6d846ee9a72ee484a81f8a01c37e0c85f71c2dae
Laurent <lg390@cam.ac.uk>
2016-12-27 19:07:56 +0000

    add utils unit tests

---
+++ tests/testthat/test_utils.R
@@ -0,0 +1,20 @@
+context("testing utils")
+
+ac <- "P27361"
+
+test_that("upNames", {
+    x <- UniProtProtein(ac)
+    nms0 <- c("accession", "name", "protein", "gene", "organism",
+              "reference", "comment", "dbReference",
+              "proteinExistence", "keyword", "feature", "evidence",
+              "sequence")    
+    expect_identical(UniProt.REST:::upNames(x), nms0)
+})
+
+
+test_that("upNames", {
+    x <- UniProtProtein(ac)
+    accs <- c("P27361", "A8CZ58", "B0LPG3", "Q8NHX1")
+    expect_identical(UniProt.REST:::upGet(x, "accession"), accs)
+    expect_error(UniProt.REST:::upGet(x, "accessions"))
+})
