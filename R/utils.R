43cdd15c53ac5c916bcc26d02cea8ddd18137320
Laurent <lg390@cam.ac.uk>
2016-12-27 21:12:26 +0000

    re-run test, with modified fun

--- R/utils.R
+++ R/utils.R
@@ -2,13 +2,13 @@ upNames <- function(object)
     unique(names(object@Data))
 
 upGet <- function(object, what) {
-    sel <- names(object@Data) %in% what
-    if (!any(sel)) {
+    k <- grep(what, names(object@Data))
+    if (!length(k)) {
         err <- paste0("'", what, "' not found. Use any of\n",
                       paste(paste(strwrap(paste(upNames(object), collapse = " "))),
                             collapse = "\n"),
-                      "\n")
+                      "\n")     
         stop(err)
     }
-    unlist(object@Data[sel], use.names = FALSE)
+    unlist(object@Data[k], use.names = FALSE)
 }
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -16,6 +16,6 @@ test_that("upGet", {
     x <- UniProtProtein(ac)
     expect_error(UniProt.REST:::upGet(x, "accessions"))
     accs0 <- c("P27361", "A8CZ58", "B0LPG3", "Q8NHX1")
-    ## accs <- UniProt.REST:::upGet(x, "accession")
-    ## expect_identical(accs, accs0)
+    accs <- UniProt.REST:::upGet(x, "accession")
+    expect_identical(accs, accs0)
 })
